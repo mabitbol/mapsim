a82a45add336d2c49085e8b657706e7e70ac74a7
Maximilian H Abitbol <mabitbol15@gmail.com>
2016-04-05 22:05:22 -0400

    ebex pointing

--- ebex_sim.py
+++ ebex_sim.py
@@ -10,22 +10,23 @@ import fits_writer
 import seaborn as sns
 from pylab import cos, sin, pi, e, arctan2, sqrt
 
+import glob
+import deepdish as dd
+
 from tools_glp import *
 
 from leap.lib.units.angles import *
 from leap.lib.geometry import coordinates
 from leap.lib.mapping import naive_binning
+from leap.resources.constants import sample_rates
+rate = sample_rates.bolo
 
 class glp_sim():
 
     def run(self):
         nside = 512
         num_det = 2
-        fsamp = 11. #Hz
-        dt = 1/fsamp 
-        days = 10
-        lat = from_degrees(-75)
-        omega = from_degrees(0.5)          #rad per second
+        dt = 1/rate
 
         self.polarization = False
         hwp_hz = 1.26*pi/e
@@ -35,35 +36,41 @@ class glp_sim():
         fknee = 0.05             #Hz
         alpha = 2.
         beam = 8.0              #arcmins
-        beam = from_degrees(beam/60.)
+        beam = from_arcmin(beam)
 
         self.cmb_on = True
         self.plmaps = True
-        idn = "red25a2fk05"
+        idn = "ebextest"
         self.savedir = make_dir(idn)
-        save_name = self.savedir+idn+"_nside"+str(nside)+"_days"+str(days)+\
-                    "_net"+str(net)+"_fk"+str(int(fknee))
+        save_name = self.savedir+idn+"_nside"+str(nside)+"_net"+str(net)+\
+                    "_fk"+str(int(fknee))
         ##########################################################
 
         tic = timeit.default_timer()
-        N_sec = 24*60*60
-        N = int(N_sec / dt) 
         skymap = create_cmb(nside, self.polarization)
         T, Q, U, hits = np.zeros((4,hp.nside2npix(nside)))
         delete_txt(self.savedir)
-        for i in range(days):
+        
+        pointing_dir = 'galaxy_cut/'
+        pointing_files = glob.glob(pointing_dir+'2012-*')
+        for i, pfile in enumerate(pointing_files):
             if self.polarization:
-                T1, Q1, U1, hits1 = self.run_on_day(N, omega, hwp_speed,\
-                                dt, lat, beam, nside, skymap, net,\
-                                fknee, alpha, num_det, save_name, i)
-                T += T1
-                Q += Q1
-                U += U1
-                hits += hits1
+                print "no pol"
+                #T += T1
+                #Q += Q1
+                #U += U1
+                #hits += hits1
+                return
             else:
-                T1, hits1 = self.run_on_day(N, omega, hwp_speed, dt,\
-                            lat, beam, nside, skymap, net, fknee,\
-                            alpha, num_det, save_name, i)
+                pointing, N = self.load_pointing(pfile)
+                data, offsets = self.get_tod(nside, skymap, pointing, dt,\
+                            net, fknee, alpha)
+                mjds = get_timing(pointing['time'])
+                flags = np.ones(N)
+                hdulist = fits_writer.make_fits(N, mjds, data, pointing,\
+                            offsets, num_det, flags, net, fknee, alpha)
+                fits_writer.write_copy_fits(hdulist, save_name+str(i), self.savedir)
+                T1, hits1 = self.make_maps(nside, pointing, data, num_det)
                 T += T1
                 hits += hits1
         T /= hits
@@ -74,47 +81,27 @@ class glp_sim():
         print "time ", timeit.default_timer()-tic
         return
 
-    def run_on_day(self, N, omega, hwp_speed, dt, lat, beam, nside,\
-                   skymap, net, fknee, alpha, num_det, save_name, i):
-        start = i*N    
-        pointing = self.get_pointing_day(start, omega, hwp_speed,\
-                        dt, lat, beam)
-        data, offsets = self.get_tod(nside, skymap, pointing, dt,\
-                            net, fknee, alpha)
-        mjds = get_timing(pointing['time'])
-        flags = np.ones(N)
-        hdulist = fits_writer.make_fits(N, mjds, data, pointing,\
-                    offsets, num_det, flags, net, fknee, alpha)
-        fits_writer.write_copy_fits(hdulist, save_name+str(i), self.savedir)
-        return self.make_maps(nside, pointing, data, num_det)
-
-    def get_pointing_day(self, start, omega, hwp_speed, dt, lat, beam):
-        #start on equator. elevation goes continuously 0 to pi/2 in a day
-        #az does circles about elevation.  earth rotates around
-        print "creating pointing"
-        lst_rad_per_sec = 7.292117834034606e-05  #equals omega earth
-        N_sec = 24*60*60
-        N = int(N_sec / dt)
-
+    def load_pointing(self, pfile):
+        data = dd.io.load(pfile)
+        times = data[0]['times']
+        lats = data[0]['lats']
+        lons = data[0]['lons']
+        
         pointing = {}
-        time = start+np.arange(N)*dt      
-        lsts = time*lst_rad_per_sec
-        az = omega*time
-        az %= (2*pi)
-        el = np.ones(N)*from_degrees(65)
-        ra, dec = my_hor_to_eq(az,el,lat,lsts)
-        ra, dec = coordinates.eq_to_gal(ra,dec)
-        hwp = (hwp_speed*time)%(2*pi)   
+        pointing['time'] = times
+        pointing['theta'] = pi/2. - lats
+        pointing['phi'] = lons
+
+        N = len(times)
         roll = np.zeros(N)
-        pointing['time'] = time
-        pointing['theta'] = pi/2. - dec
-        pointing['phi'] = ra
         pointing['roll'] = roll
         if self.polarization:
-            pointing['hwp'] = hwp
+            # fix for polarization
+            #hwp = (hwp_speed*time)%(2*pi)   
+            pointing['hwp'] = roll
         else:
             pointing['hwp'] = roll
-        return pointing
+        return pointing, N
 
     def get_tod(self, nside, skymap, pointing, dt, net, fknee, a):
         print "calculating tod and noise"
@@ -140,7 +127,6 @@ class glp_sim():
         N = len(tod)
         fft_len = int(2**(2+np.floor(np.log2(N))))
         fnoise = pink_noise.pink_noise(length=fft_len, delta_t=dt, net=net, fknee=fknee, alpha=a, show_plot=False)
-        #fnoise2 = pink_noise.pink_noise(length=fft_len, delta_t=dt, net=net, fknee=fknee, alpha=a, show_plot=False)
         x = np.random.randint(1,N/2)
         tod1 = tod + fnoise[x:N+x]
         x = np.random.randint(N/2,fft_len-N-1)
@@ -153,32 +139,24 @@ class glp_sim():
         print "adding maps"
         binning_func = naive_binning.add_to_hit_and_unnormalized_signal_map
         Nl = hp.nside2npix(nside)
-        #pix = hp.ang2pix(nside, pointing['theta'], pointing['phi'])
-        #length = len(pix)
         dmoney = (data[0] + data[1])/2.
 
         I = np.zeros(Nl, np.double) 
-        Q = np.zeros(Nl, np.double)
-        U = np.zeros(Nl, np.double)
         hits = np.zeros(Nl, np.uint64)
-        hits2 = np.zeros(Nl, np.uint64)
         lat = pi/2. - pointing['theta']
         lon = pointing['phi']
         if self.polarization:
+            Q = np.zeros(Nl, np.double)
+            U = np.zeros(Nl, np.double)
+            hits2 = np.zeros(Nl, np.uint64)
             qdata = 0.5*dmoney*cos(4*pointing['hwp']+2*pointing['roll'])
             udata = 0.5*dmoney*sin(4*pointing['hwp']+2*pointing['roll'])
             binning_func(hits, I, dmoney, lon, lat)
             binning_func(hits2, Q, qdata, lon, lat)
             binning_func(hits2, U, udata, lon, lat)
-            #I = np.bincount(pix, weights=dmoney, minlength=Nl)
-            #Q = np.bincount(pix, weights=qdata, minlength=Nl)
-            #U = np.bincount(pix, weights=udata, minlength=Nl)
-            #hits = np.bincount(pix, weights=np.ones(length)*num_det, minlength=Nl)
             return I, Q, U, hits
         else:
             binning_func(hits, I, dmoney, lon, lat)
-            #I = np.bincount(pix, weights=dmoney, minlength=Nl)
-            #hits = np.bincount(pix, weights=np.ones(length)*num_det, minlength=Nl)
             return I, hits
 
     def plot_maps(self, I, Q, U, hits, idn):
@@ -218,7 +196,6 @@ class glp_sim():
             pl.title("U map")
             pl.savefig(idn+"Umap")
             pl.close()
-
         return
         
 
@@ -228,13 +205,3 @@ if __name__ == "__main__":
     simulation.run()
 
 
-
-#el = np.ones(N)*pi/2.
-#el_speed = (pi/2.)/(24.*60.*60.)
-#el = el_speed*time
-#el %= pi 
-#x = el>(pi/2.)
-#el[x] = pi - el[x]
-#el %= (pi - 2*res)    #minus res here so that we dont point straight up.
-#x = el>(pi/2. - res)
-#el[x] = (pi - 2*res) - el[x]
---
+++ view_pointing.py
@@ -0,0 +1,99 @@
+import numpy as np
+import healpy as hp
+import matplotlib as mpl
+mpl.use('Agg')
+import pylab as pl
+import glob
+import deepdish as dd
+from leap.lib.units.angles import *
+import os
+
+def plot_hitmap():
+    pointing_dir = "ebex_pointing/"
+    segments = glob.glob(pointing_dir+'segment*')
+    for seg in segments:
+        boards = glob.glob(seg+'/board*')
+        for board in boards:
+            bolos = glob.glob(board+'/2012-*')
+            for bolo in bolos:
+                data = read_bolo_pointing(bolo)
+                plot_bolo_data(data)
+
+
+
+
+# deepdish pointing files save as list with dictionaries in them.
+# should change this to use h5py asap
+# [segname, bolo_name, times, valids, lats, lons, int: frequency]
+def read_bolo_pointing(fname):
+    data = dd.io.load(fname)
+    segment = str(data[0]['segname'])
+    bolo_name = str(data[1]['bolo_name'])
+    times = data[2]['times']
+    # 1 is good for valid
+    valid = data[3]['valids'].astype('bool')
+    lats = data[4]['lats']
+    lons = data[5]['lons']
+    freq = str(data[6])
+    return times, valid, lats, lons, freq, segment, bolo_name 
+
+
+def plot_bolo_data(data):
+    times, valid, lats, lons, freq, segment, bolo_name = data
+    pl.figure()
+    pl.plot(times[valid], to_degrees(lats[valid])+90., 'b,')
+    pl.plot(times[~valid], to_degrees(lats[~valid])+90., 'r,')
+    pl.plot(times[valid], to_degrees(lons[valid]), 'g,')
+    pl.plot(times[~valid], to_degrees(lons[~valid]), 'r,')
+    pl.xlim([0,10000])
+    pl.ylabel('pointing [deg]')
+    pl.xlabel('time [s]')
+    pl.show()
+
+
+
+def get_galcut(times, lats, lons, valid):
+    gc = lats < from_degrees(5.)
+    gc &= lats > from_degrees(-5.)
+    gc &= valid
+    return times[gc], lats[gc], lons[gc]
+
+def save_data(fname, times, lats, lons):
+    dd.io.save(fname+'.h5', [{"times": times, "lats": lats,\
+               "lons": lons}])
+    return
+    
+
+def galaxy_cut():
+    pointing_dir = "ebex_pointing/"
+    galcut_dir = "galaxy_cut"
+    make_dir(galcut_dir)
+    segments = glob.glob(pointing_dir+'segment*')
+    for seg in segments:
+        boards = glob.glob(seg+'/board*')
+        for board in boards:
+            bolos = glob.glob(board+'/2012-*')
+            for bolo in bolos:
+                data  = read_bolo_pointing(bolo)
+                times, valid, lats, lons, freq, segname, bolo_name = data
+                times, lats, lons = get_galcut(times, lats, lons, valid)
+                if len(times):
+                    fname = galcut_dir+'/'+segname+'_'+bolo_name+'_'+freq
+                    save_data(fname, times, lats, lons)
+                else:
+                    print "no valid galaxy pointing"
+    return 
+
+
+
+def make_dir(dir_name, name=""):
+    path = dir_name+"/"+name
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+    return path
+
+
+galaxy_cut()
